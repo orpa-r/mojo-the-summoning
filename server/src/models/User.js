35f495b46b41b18893048c6a9d1d5f084c62861d
orpa-r <89654380+orpa-r@users.noreply.github.com>
2023-05-05 10:30:43 +0000

    models and associations

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,34 @@
 // create your User model here
+let User = sequelize.define("User", {
+    id: Sequelize.INTERGER,
+    name: Sequelize.STRING,
+    xp: Sequelize.INTERGER
+});
+
+let Deck = sequelize.define("Deck", {
+    id: Sequelize.INTERGER,
+    name: Sequelize.STRING,
+    xp: Sequelize.INTERGER
+});
+
+let Card = sequelize.define("Card", {
+    id: Sequelize.INTERGER,
+    name: Sequelize.STRING,
+    mojo: Sequelize.INTERGER,
+    stamina: Sequelize.INTERGER,
+    imgUrl: Sequelize.STRING
+});
+
+let Attack = sequelize.define("Attack", {
+    id: Sequelize.INTERGER,
+    title: Sequelize.STRING,
+    mojoCost: Sequelize.INTERGER,
+    staminaCost: Sequelize.INTERGER
+});
+
+module.exports = {
+    User,
+    Deck,
+    Card,
+    Attack
+};
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,7 +1,18 @@
 const { User } = require('./User')
 // import the rest of your models above
+const { Deck } = require('./User')
+const { Card } = require('./User')
+const { Attack } = require('./User')
 
 //set up the associations here
+User.hasOne(Deck);
+Deck.hasMany(Card);
+Card.hasOne(Deck);
+Card.hasMany(Attack);
+Attack.belongsToMany(Card);
 
 // and then export them all below
 module.exports = { User }
+module.exports = { Deck }
+module.exports = { Card }
+module.exports = { Attack }
