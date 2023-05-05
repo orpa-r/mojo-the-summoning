591945424564668711404f029af544bdec3fd708
orpa-r <89654380+orpa-r@users.noreply.github.com>
2023-05-05 12:04:49 +0000

    database made

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,14 @@
 // add your database connection here
+const {Sequelize} = require("sequelize");
+const path = require("path");
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, "db.sqlite")
+});
+
+
+module.exports = {
+    sequelize,
+    Sequelize
+}
